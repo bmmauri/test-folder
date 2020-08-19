e0de7276e77e2b4dc1ce6d3d9e878f16e4a44b47
Maurizio <maurizio.bussi.lavoro@gmail.com>
2020-08-19 06:17:32 +0200

    init with core engine

---
+++ tests/test_engine.py
@@ -0,0 +1,66 @@
+import random
+import unittest
+
+from tf.core.engine import Machine, Action, MachineState
+
+
+class EngineTestCase(unittest.TestCase):
+    def test__machine_multiple_actions(self):
+        """
+        Test the number of attachments into the machine.
+        """
+        machine = Machine()
+        attachments = [machine.attach(Action()) for i in range(random.randint(2,19))]
+        self.assertEqual(len(machine._actions), len(attachments))
+
+    def test__machine_abort(self):
+        """
+        Test the state of the machine: MachineState.ABORT.
+        """
+        machine = Machine()
+        machine.abort()
+        self.assertEqual(machine._machine_state, MachineState.ABORT)
+
+    def test__machine_start(self):
+        """
+        Test the state of the machine: MachineState.START.
+        """
+        machine = Machine()
+        machine.start()
+        self.assertEqual(machine._machine_state, MachineState.START)
+
+    def test__machine_run(self):
+        """
+        Test the state of the machine: MachineState.RUN.
+        """
+        machine = Machine()
+        machine.run()
+        self.assertEqual(machine._machine_state, MachineState.RUN)
+
+    def test__machine_pause(self):
+        """
+        Test the state of the machine: MachineState.PAUSE.
+        """
+        machine = Machine()
+        machine.pause()
+        self.assertEqual(machine._machine_state, MachineState.PAUSE)
+
+    def test__machine_block(self):
+        """
+        Test the state of the machine: MachineState.BLOCK.
+        """
+        machine = Machine()
+        machine.block()
+        self.assertEqual(machine._machine_state, MachineState.BLOCK)
+
+    def test__machine_finish(self):
+        """
+        Test the state of the machine: MachineState.FINISH.
+        """
+        machine = Machine()
+        machine.finish()
+        self.assertEqual(machine._machine_state, MachineState.FINISH)
+
+
+if __name__ == '__main__':
+    unittest.main()
