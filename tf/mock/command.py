c36b84570a62e7d7950bc7bcff535afd1536a1d1
Maurizio <maurizio.bussi.lavoro@gmail.com>
2020-08-26 22:08:25 +0200

    update stop, pause, abort command

--- tf/mock/command.py
+++ tf/mock/command.py
@@ -1,14 +1,52 @@
 class Command:
     """Generic command object"""
-    pass
+
+    __command = None
+
+    def __init__(self, command) -> None:
+        super().__init__()
+        self.__command = command
+
+    def get_command(self):
+        return self.__command
 
 
 class Start(Command):
     """Start command object"""
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, command: str = None) -> None:
+        super().__init__(command=command)
 
     def __str__(self):
-        return "START"
+        return "START" if self.get_command() is None else f"START - {self.get_command()}"
+
+
+class Stop(Command):
+    """Start command object"""
+
+    def __init__(self, command: str = None) -> None:
+        super().__init__(command=command)
+
+    def __str__(self):
+        return "STOP" if self.get_command() is None else f"STOP - {self.get_command()}"
+
+
+class Pause(Command):
+    """Start command object"""
+
+    def __init__(self, command: str = None) -> None:
+        super().__init__(command=command)
+
+    def __str__(self):
+        return "PAUSE" if self.get_command() is None else f"PAUSE - {self.get_command()}"
+
+
+class Abort(Command):
+    """Start command object"""
+
+    def __init__(self, command: str = None) -> None:
+        super().__init__(command=command)
+
+    def __str__(self):
+        return "ABORT" if self.get_command() is None else f"ABORT - {self.get_command()}"
 
